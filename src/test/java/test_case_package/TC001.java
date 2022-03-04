d02ab25affa8172973b7107885574e7e7445f846
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-03-04 15:53:46 +0600

    Update

--- src/test/java/test_case_package/TC001.java
+++ src/test/java/test_case_package/TC001.java
@@ -1,5 +1,6 @@
 package test_case_package;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import driver_package.BaseDriver;
@@ -12,6 +13,11 @@ public class TC001 extends BaseDriver{
 	public void locator() throws InterruptedException {
 		
 		driver.get(baseUrl);
+		driver.manage().window().maximize();
+		
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[normalize-space()='Sign In']")).click();
+		
 		Thread.sleep(2000);
 	}
 
