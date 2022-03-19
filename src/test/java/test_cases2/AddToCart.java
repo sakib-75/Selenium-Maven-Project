342056f2da888d6448d92c9f2c503b2f777d67a5
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-03-19 12:31:41 +0600

    Update

--- src/test/java/test_cases2/AddToCart.java
+++ src/test/java/test_cases2/AddToCart.java
@@ -8,11 +8,9 @@ import org.testng.annotations.Test;
 
 import driver_package.BaseDriver;
 import page_object_model.TestCases2POM;
-import utilities_package.ImplicitlyWait;
-import utilities_package.TakeScreenshot;
-import utilities_package.WarningMessage;
+import utilities_package.Utilities;
 
-public class AddToCart extends BaseDriver implements WarningMessage, ImplicitlyWait, TakeScreenshot {
+public class AddToCart extends BaseDriver implements Utilities {
 
 	@Test(priority = 1)
 	public void addToCart() {
