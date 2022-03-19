90791dc60785733c92906899db3686fc5577bcbc
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-03-19 12:36:11 +0600

    Update window handling

--- src/test/java/action_test/WindowsHandling.java
+++ src/test/java/action_test/WindowsHandling.java
@@ -7,36 +7,35 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import driver_package.BaseDriver;
-import utilities_package.ImplicitlyWait;
+import utilities_package.Utilities;
 
-public class WindowsHandling extends BaseDriver implements ImplicitlyWait {
+public class WindowsHandling extends BaseDriver implements Utilities {
 
 	@Test
 	public void window() throws InterruptedException {
 
 		driver.get("https://sakibul-islam.netlify.app");
 		implicitWait(driver);
-		
+
 		driver.findElement(By.xpath("//section[@id='about']//div[3]//a[1]")).click();
 		driver.findElement(By.xpath("//section[@id='about']//div[4]//a[1]")).click();
 		System.out.println(driver.getTitle());
-		
+
 		Set<String> tab = driver.getWindowHandles();
 		Iterator<String> itr = tab.iterator();
-		
+
 		String parTab = itr.next();
 		String chTab1 = itr.next();
 		String chTab2 = itr.next();
-		
+
 		driver.switchTo().window(chTab1);
 		System.out.println(driver.getTitle());
-		
+
 		driver.switchTo().window(chTab2);
 		System.out.println(driver.getTitle());
-		
+
 		driver.switchTo().window(parTab);
 		System.out.println(driver.getTitle());
-	
 
 	}
 
